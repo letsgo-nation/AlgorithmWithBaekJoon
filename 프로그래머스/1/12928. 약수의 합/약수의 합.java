e1b8f030b7002c387a6ebca4576802da575a1a4c
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-03-01 08:55:02 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n ; i++) {
+            if (n%i==0){
+            answer += i;
+            }
+        }
+        return answer;
+    }
+}
+
+//public class Main {
+//     public static void main(String[] args) {
+//         int n = 12;
+//         int sum = 0;
+//         for (int i = 1; i <= n ; i++) {
+//             if (n%i==0){
+//                 System.out.print(i);
+//                 sum += i;
+//                 System.out.println(sum);
+//             }
+//         }
+//     }
+// }
