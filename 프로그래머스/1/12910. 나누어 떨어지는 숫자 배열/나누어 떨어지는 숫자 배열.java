25ef501516870a6963e36773086b174cd5b5baef
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-03-23 21:12:33 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 3.22 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,19 +1,29 @@
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
     public int[] solution(int[] arr, int divisor) {
         
-        // divisor로 나누어 떨어지는 값을 담을 배열 생성
-        int[] answer = Arrays.stream(arr)
-                            .filter(x -> x % divisor == 0) // divisor로 나누어 떨어지는 값 필터링
-                            .sorted() // 정렬
-                            .toArray(); // 배열로 변환
-        
-        // divisor로 나누어 떨어지는 값이 없는 경우
-        if (answer.length == 0) {
-            return new int[]{-1};
+        int count = 0;
+        Queue<Integer> que = new LinkedList<>();
+
+        for (int i=0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                que.add(arr[i]);
+                count++;
+            }
         }
         
-        return answer; 
+        if (count==0) {
+            return new int[] {-1};
+        }
+        
+        int[] answer = new int[count];
+        for(int i=0; i<count; i++) {
+            answer [i] = que.poll();
+        }
+        answer = Arrays.stream(answer).sorted().toArray();
+        return answer;
     }
 }
