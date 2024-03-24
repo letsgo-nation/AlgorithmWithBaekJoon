7fcbd37086e04a39e98e471bc90db265eb57d473
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-03-24 23:45:32 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 1.25 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        // 1. 문자열을 char 배열로 변환 + 저장 
+        // 2. 배열 갯수가 짝수이면 /2 번째 캐릭 가져오기 <> 홀수이면 /2 2개의 캐릭 가져오기
+        
+        // // 1-1. 반복문
+        // char[] charS = new char [s.length()];
+        // for (int i=0; i<s.length(); i++) {
+        //     charS[i] = s.charAt(i);
+        // }
+        
+        // 1-2. toCharArray
+        char[] charS = s.toCharArray();
+        
+        // 짝수&홀수 함수
+        if (s.length()%2==0) {
+            answer = String.valueOf(charS[s.length()/2-1]) + String.valueOf(charS[s.length()/2]);
+        } else {
+            answer = String.valueOf(charS[s.length()/2]);
+        }
+        return answer;
+    }
+}
