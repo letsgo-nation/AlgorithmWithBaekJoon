4411d0e7e97ebb8faafb835deaab08c8a43ba1f8
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-03-26 16:17:57 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -2,12 +2,25 @@ class Solution {
     public boolean solution(String s) {
         boolean answer = true;
           
+        // s의 길이가 4 또는 6
         if (!(s.length()==4 || s.length()==6)) {
             answer = false;
         }
         
+        // 1. chartAt()을 활용하여 숫자 체크하기
+//        for(int i=0; i<s.length(); i++) {
+//            if (!('0' <= s.charAt(i) && s.charAt(i) <= '9')) {
+//                answer = false;
+//            }
+//        }
+
+        // 2. isDigit() 함수 활용하기
+        char check;
+
         for(int i=0; i<s.length(); i++) {
-            if (!('0' <= s.charAt(i) && s.charAt(i) <= '9')) {
+            check = s.charAt(i);
+
+            if (!Character.isDigit(check)) {
                 answer = false;
             }
         }
