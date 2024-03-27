5204e449ace21c29f4ed310db00ffc18cee953ae
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-03-27 11:13:28 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.59 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,19 +1,11 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String[] seoul) {
-        String str1 = "김서방은 ";
-        String str2 = "에 있다";
-        String x = "";
-        
-        for (int i=0; i<seoul.length; i++) {
-            if(seoul[i].equals("Kim")) {
-                x = String.valueOf(i);
-            }
-        }
-    
-        String answer = str1+x+str2;
-        return answer;
-    }
-}
 
-// 소요시간 :  48분
-// return x (the location of Kim in array);
+    return "김서방은 "+ Arrays.asList(seoul).indexOf("Kim") + "에 있다";
+    }
+//         int x = Arrays.asList(seoul).indexOf("Kim");
+
+//         return "김서방은 "+x+"에 있다";
+}
