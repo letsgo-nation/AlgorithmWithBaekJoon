f7ae643ad84adf2a7d0ce12498f0ba5ca2831c4c
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-03-27 17:55:28 +0900

    [level 1] Title: 행렬의 덧셈, Time: 4.70 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,20 +1,32 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int rows = arr1.length;
-        int cols = arr1[0].length;
-        
-        int[][] answer = new int[rows][cols];
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                answer[i][j] = arr1[i][j] + arr2[i][j];
+        int[][] answer = {};
+        answer = arr1;
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr1[0].length; j++){
+                answer[i][j] += arr2[i][j];
             }
         }
-        
         return answer;
     }
 }
+        
+        
+//         int rows = arr1.length;
+//         int cols = arr1[0].length;
+        
+//         int[][] answer = new int[rows][cols];
+        
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 answer[i][j] = arr1[i][j] + arr2[i][j];
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
 
 // 1hr
