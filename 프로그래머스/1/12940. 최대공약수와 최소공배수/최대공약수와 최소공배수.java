4cad57be642bfda7d555e49b841eae3367e777cd
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-03-28 22:20:07 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.06 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int[] solution(int n, int m) {
+       int[] answer = new int[2];
+        // 최대공약수
+        for (int i=1; i<=Math.min(n,m);i++) {
+            if (n%i==0 && m%i==0) {
+                answer[0] = i;
+            }
+        }
+        // 최소공배수
+        answer[1] = n*m/answer[0];
+        return answer;
+    }
+}
