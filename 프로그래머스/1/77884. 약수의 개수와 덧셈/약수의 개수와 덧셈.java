5e4ba96366340a3c4807b3ab3cdd52be4b2c78f5
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-04-03 09:37:01 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.07 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -4,12 +4,14 @@ class Solution {
         int[] arr = new int[right-left+1];
         int[] arr1 = arr.clone();
         arr[0] = left;
+        
         int count = 0;
         int answer = 0;
 
         for (int i=1; i<arr.length; i++) {
             arr[i] = arr[i-1]+1;
         }
+        
         for (int i=0; i<arr.length; i++) {
             for (int j=1;j*j<=arr[i];j++) {
                 if(arr[i]%j==0) {
@@ -22,14 +24,13 @@ class Solution {
             arr1[i] = count;
             count = 0;
         }
+        
         for(int i=0; i<arr.length ; i++) {
             if(arr1[i]%2==1) {
                 arr[i] = -arr[i];
             }
-            answer = answer + arr[i];
+        answer = answer + arr[i];
         }
         return answer;
     }
 }
-
-//  소요시간 : 45분
