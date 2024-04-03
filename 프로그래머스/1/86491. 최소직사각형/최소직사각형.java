b39127c44bbcff3b0ac4681ecf1001a074c65f53
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-04-03 17:19:23 +0900

    [level 1] Title: 최소직사각형, Time: 1.39 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -3,6 +3,7 @@ class Solution {
         int maxWidth = 0;
         int maxHeight = 0;
 
+        // 가로의 최댓값과 세로의 최댓값 구하기 
         //  for (int i = 0; i < sizes.length; i++) {
         // int width = sizes[i][0];
         // int height = sizes[i][1];
