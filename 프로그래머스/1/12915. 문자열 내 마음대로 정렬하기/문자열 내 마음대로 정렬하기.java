c52359ae10d9faac8edc96de5dce1cca9d0ab996
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-04-04 18:21:05 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.55 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,32 +1,22 @@
-import java.util.Arrays;
 class Solution {
-    public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length];
+  public String[] solution(String[] strings, int n) {
 
-        // 현재 배열 복사
-        for (int i = 0; i < strings.length; i++) {
-            answer[i] = strings[i];
-        }
-
-        for (int i = 0; i < answer.length; i++) {
-            for (int j = i + 1; j < answer.length; j++) {
-                // 인덱스 n의 문자를 기준으로 비교하여 정렬
-                if (answer[i].charAt(n) > answer[j].charAt(n)) {
-                    String temp = answer[i];
-                    answer[i] = answer[j];
-                    answer[j] = temp;
-                }
-                // 만약 인덱스 n의 문자가 같다면 사전순으로 정렬
-                if (answer[i].charAt(n) == answer[j].charAt(n)) {
-                    if (answer[i].compareTo(answer[j]) > 0) {
-                        String temp = answer[i];
-                        answer[i] = answer[j];
-                        answer[j] = temp;
-                    }
-                }
-            }
-        }
-
-        return answer;
-    }
+      for(int i=0; i<strings.length-1; i++){
+          for(int k=i+1; k<strings.length;k++){
+              if(strings[i].charAt(n)>strings[k].charAt(n)){ 
+                  String tmp = strings[i];
+                  strings[i]= strings[k];
+                  strings[k]=tmp;
+              }
+              else if(strings[i].charAt(n)==strings[k].charAt(n)){
+                  if(strings[i].compareTo(strings[k]) >0){ 
+                      String tmp = strings[i];
+                      strings[i]= strings[k];
+                      strings[k]=tmp;
+                  }
+              }
+          }
+      }
+      return strings;
+  }
 }
