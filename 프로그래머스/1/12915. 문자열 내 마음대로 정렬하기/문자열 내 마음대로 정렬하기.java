854ef872751f244de21f720fef9ee70811068485
letsgo-nation <131455266+letsgo-nation@users.noreply.github.com>
2024-04-04 18:26:48 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.62 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -2,17 +2,18 @@ class Solution {
   public String[] solution(String[] strings, int n) {
 
       for(int i=0; i<strings.length-1; i++){
-          for(int k=i+1; k<strings.length;k++){
-              if(strings[i].charAt(n)>strings[k].charAt(n)){ 
+          for(int j=i+1; j<strings.length; j++){
+              if(strings[i].charAt(n)>strings[j].charAt(n)){ 
                   String tmp = strings[i];
-                  strings[i]= strings[k];
-                  strings[k]=tmp;
+                  strings[i]= strings[j];
+                  strings[j]=tmp;
               }
-              else if(strings[i].charAt(n)==strings[k].charAt(n)){
-                  if(strings[i].compareTo(strings[k]) >0){ 
+              // 같은 문자열일 경우
+              else if(strings[i].charAt(n)==strings[j].charAt(n)){
+                  if(strings[i].compareTo(strings[j]) >0){ 
                       String tmp = strings[i];
-                      strings[i]= strings[k];
-                      strings[k]=tmp;
+                      strings[i]= strings[j];
+                      strings[j]=tmp;
                   }
               }
           }
@@ -20,3 +21,8 @@ class Solution {
       return strings;
   }
 }
+
+// compareTo
+// 두 문자열이 동일한 경우에는 0을 반환
+// 첫 번째 문자열이 두 번째 문자열보다 사전 순으로 앞에 올 경우, 양수를 반환
+// 첫 번째 문자열이 두 번째 문자열보다 사전 순으로 뒤에 올 경우, 음수를 반환
