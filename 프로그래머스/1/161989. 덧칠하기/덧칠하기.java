1f8ffaa2af04a70e7fe108b3876036c851f5500f
Won So <131455266+letsgo-nation@users.noreply.github.com>
2024-04-19 22:16:05 +0900

    [level 1] Title: 덧칠하기, Time: 1.08 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        // 1. 1~n
-        // 2. 덧칠해야하는 부분
-        // the lowest elemen+m 끊고 
-        // 다시 
-        // 2+4-1 =6
-        // 10 4 {2,3,/ 6,7,9,/ 10} = 3
         int count = 1; 
         
         for(int i=1; i<section.length; i++) {
