4df51442d2b086e5a79c61d781eb0d9e18d7b340
Won So <131455266+letsgo-nation@users.noreply.github.com>
2024-04-21 00:20:16 +0900

    [Silver III] Title: 파도반 수열, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,24 +1,24 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args)throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-        long DP[] = new long[101];
+        long [] dp = new long[101];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
 
-        DP[0] = 0;
-        DP[1] = 1;
-        DP[2] = 1;
+        for(int i=0; i<T; i++) {
+            int n = Integer.parseInt(br.readLine());
 
-        for(int i=0;i<T;i++) {
-            int N = sc.nextInt();
-
-            for(int j=3;j<=N;j++)
-                DP[j] = DP[j-3]+DP[j-2];
-
-            System.out.println(DP[N]);
+            for(int j=3; j<=n; j++) {
+                dp[j] = dp[j-2]+dp[j-3];
+            }
+            System.out.println(dp[n]);
         }
     }
 }
